f4c2d74fb5e1361a602bb1e4253e709b7a7c7d53
Mirelly <mirelly@DESKTOP-HNLVMP3.localdomain>
2023-01-24 19:22:26 -0300

    :sparkles: first commmit

---
+++ script-static.sh
@@ -0,0 +1,30 @@
+RTR=rtr.jar
+HWSW=static/andre_cunha_
+
+# Executa os roteadores
+tmux new-session -d -s rare 'java -jar '$RTR' routersc '$HWSW'r1-hw.txt '$HWSW'r1-sw.txt'
+tmux split-window -v -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r2-hw.txt '$HWSW'r2-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r3-hw.txt '$HWSW'r3-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r4-hw.txt '$HWSW'r4-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r5-hw.txt '$HWSW'r5-sw.txt' ENTER;
+tmux select-layout tiled;
+
+echo "Iniciando roteadores..."
+sleep 3 # Espera 3 segundos para iniciar os roteadores
+
+tmux new-session -d -s telnet 'telnet localhost 26001'
+tmux split-window -v -t 0 -p 50
+tmux send 'telnet localhost 26002' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'telnet localhost 26003' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'telnet localhost 26004' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'telnet localhost 26005' ENTER;
+tmux select-layout tiled;
+
+tmux a;
